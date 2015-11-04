c93941a0fd0394f5e9cee68d142c03da1bdc256f
Ashish kale <ashishkale0407@gmail.com>
2015-11-04 16:55:48 +0530

    change in hello file added 3 sysout

--- JavaProject/src/hello/Hello.java
+++ JavaProject/src/hello/Hello.java
@@ -10,7 +10,10 @@ public class Hello {
 		System.out.println("Test 3");
 		
 		System.out.println("Test 4");
-		
+		System.out.println("Test 3");
+		System.out.println("Test 0");
+		System.out.println("Test 1");
+		System.out.println("Test 2");
 		
 		
 
