3851498473779b46e61db2c896ad011ed8802d5a
Ashish kale <ashishkale0407@gmail.com>
2015-11-24 11:35:49 +0530

    inital commit

---
+++ demo1/src/main/java/com/example/contact/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.example.contact;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AccountRepository extends MongoRepository<Account, String> {
+  
+  public Account findByUsername(String username);
+
+}
---
+++ demo1/src/main/java/com/example/contact/ContactRepository.java
@@ -0,0 +1,4 @@
+package com.example.contact;
+
+import org.springframework.data.mongodb.repository.MongoRepository; 
+public interface ContactRepository extends MongoRepository<Contact, String> {}
